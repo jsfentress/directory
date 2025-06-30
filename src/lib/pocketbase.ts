2689f77e7b7cb10e2549a1b4d7364ba2a81bde2b
John Fentress <64289089+jsfentress@users.noreply.github.com>
2025-06-30 11:24:37 -0500

    SB email

--- src/lib/pocketbase.ts
+++
@@ -1,7 +0,0 @@
-import PocketBase from 'pocketbase';
-
-// Create a PocketBase client instance
-export const pb = new PocketBase(import.meta.env.POCKETBASE_URL);
-
-// Export the client for use in other files
-export default pb; 
